bcac9c6470bd5380b403bdf68a43a6ebd32788c4
Rakshit  Pujari <rakshpujari@gmail.com>
2025-11-27 13:34:54 +0100

    added multer middle ware

---
+++ src/controllers/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer  from "multer";
+
+
+// storing the files in the disk 
+const storage = multer.diskStorage(
+    {
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+    }
+)
+
+export const upload = multer(
+                        { 
+                             storage, 
+                        })
+
