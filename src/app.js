eb17fab6d399fb8e4e03277777e514649a15cfd4
Rakshit  Pujari <rakshpujari@gmail.com>
2025-11-27 16:22:29 +0100

    All setup configured and basic post call made to user/register path

--- src/app.js
+++ src/app.js
@@ -23,6 +23,15 @@ app.use(cookieParser());
 // Public assets
 app.use(express.static("public"));
 
+// ----------------------------------------------
+// Routes import
+
+import userRouter from './routes/user.routes.js';
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
+// http://localhost:8000/api/v1/users/register
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import connectDB from "./db/index.js";
 import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
 import {app} from './app.js';
 
 
-dotenv.config({ 
-    path: '.env' 
-});
+dotenv.config({
+    path: './.env'
+})
 
 // execute methode to connect to db
-connectDB()
+ connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(` App is running on :: ${process.env.PORT}`)
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`⚙️ server is running on :: ${process.env.PORT}`)
     })
 })
 .catch((err)=>{
